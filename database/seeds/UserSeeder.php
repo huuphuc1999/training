bb9149e83fc88b4d34dc2a52e19acb9b85a4aa25
Phuc Huu <phan.phuc.rcvn2012@gmail.com>
2022-09-26 16:59:02 +0700

    push code

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'super admin',
+            'email' => 'superadmin@gmail.com',
+            'group_role' => 'Admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
